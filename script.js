5baf308279880ffa5bea2ec0664c5e5c8d4169ae
gaurisankartarasia <157940097+gaurisankartarasia@users.noreply.github.com>
2024-02-03 01:24:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ $(document).ready(function(){
       $('#modalImage').attr('src', '');
     });
   });
+
+  /***************Pop-Up Message ***********/
+  document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('popup');
+    setTimeout(() => {
+      popup.style.display = 'block';
+      popup.style.top = '50px';
+      popup.style.left = '50px';
+    }, 1000);
+  });
+  
+  function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.top = '-100px';
+    popup.style.left = '-100px';
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 500);
+  }
+  function redirectFeedback() {
+    // Redirect to your Google Form URL
+    window.location.href = 'https://forms.gle/CQSzW5kjLc6Kk9KZ7';
+  }
+  
